e85f57aded10b18ef688331a7c14e2bd92ecb138
Denis Ustinov <plods.codling02@icloud.com>
2025-02-03 10:00:43 +0700

    feat(config): initialize configuration with cleanenv

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/DenisUstinov/url-shortener/internal/config"
+)
+
 func main() {
-	// TODO: init config cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 
 	// TODO: init logger slog
 
